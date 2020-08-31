64a349f5fccd66f209a8419e669ff942947f66db
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-08-31 14:40:59 +0000

    created a isEven function

---
+++ lighthouse-js-fundamentals/even.js
@@ -0,0 +1,13 @@
+const isEven = function (num) {
+  return num % 2 === 0; 
+}
+// You can set the function return values to a new variable before putting them in console.log
+//const tenIsEven = isEven(10); 
+//const elevenIsEven = isEven(11); 
+
+//console.log(tenIsEven); 
+//console.log (elevenIsEven);
+
+//or you can  use them directly 
+console.log(isEven(10)); 
+console.log(isEven(11)); 
